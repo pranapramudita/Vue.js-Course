fcd3d023fc3df4af12f9ddb524df010fc3693b29
pranapramudita <pranapramudita@gmail.com>
2020-04-18 11:50:24 +0700

    delete unnecessary

--- apps.js
+++ apps.js
@@ -5,11 +5,5 @@ var app = new Vue({
         age: 20,
     },
     methods: {
-        logName: function(){
-            console.log('You entered your name')
-        },
-        logAge: function(){
-            console.log('You entered your age')
-        },
     },
 })
