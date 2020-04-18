5da92e0cbd43e077ac793eda5b242ba8122d9cfe
pranapramudita <pranapramudita@gmail.com>
2020-04-18 11:14:53 +0700

    init

--- apps.js
+++ apps.js
@@ -2,7 +2,13 @@ var app = new Vue({
     el: '#app',
     data: {
         age: 20,
-        website: 'https://www.youtube.com/playlist?list=PL4cUxeGkcC9gQcYgjhBoeQH7wiAyZNrYa',
-        websiteTags: '<a href=https://www.youtube.com/playlist?list=PL4cUxeGkcC9gQcYgjhBoeQH7wiAyZNrYa>Vue JS 2 Tutorial</a>',
+    },
+    methods: {
+        add: function(inc){
+            this.age += inc
+        },
+        click: function(){
+            alert('You clicked me')
+        },
     },
 })
