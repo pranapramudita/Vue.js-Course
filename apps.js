3aab86dbddd4d88a5bbc1730c8effeb4cb66867f
pranapramudita <pranapramudita@gmail.com>
2020-04-18 12:02:42 +0700

    init

--- apps.js
+++ apps.js
@@ -1,20 +1,17 @@
 var app = new Vue({
     el: '#app',
     data: {
-        age: 20,
-        a: 0,
-        b: 0,
+        green: false,
+        red: true,
     },
     methods: {
     },
     computed: {
-        addToA: function(){
-            console.log('addToA')
-            return this.a + this.age
-        },
-        addToB: function(){
-            console.log('addToB')
-            return this.b + this.age
+        compClasses: function(){
+            return{
+                green: this.green,
+                red: this.red
+            }
         },
     },
 })
