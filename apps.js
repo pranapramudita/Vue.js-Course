a937cbd4a90222d41ce7272d488d30faf959a98c
pranapramudita <pranapramudita@gmail.com>
2020-04-18 12:04:59 +0700

    init

--- apps.js
+++ apps.js
@@ -1,17 +1,11 @@
 var app = new Vue({
     el: '#app',
     data: {
-        green: false,
-        red: true,
+        success: false,
+        error: false,
     },
     methods: {
     },
     computed: {
-        compClasses: function(){
-            return{
-                green: this.green,
-                red: this.red
-            }
-        },
     },
 })
