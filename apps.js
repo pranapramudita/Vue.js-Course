739a023712fd71ba714bbadbabf31f2a37a9ff65
pranapramudita <pranapramudita@gmail.com>
2020-04-18 12:07:58 +0700

    init

--- apps.js
+++ apps.js
@@ -1,12 +1,6 @@
 var app = new Vue({
     el: '#app',
-    data: {        
-        characters: ['Mario','Luigi','Yoshi','Bowser'],
-        ninjas: [
-            { name: 'Ryu', age:25 },
-            { name: 'Koshi', age:30 },
-            { name: 'Yen', age:35 },
-        ]
+    data: {
     },
     methods: {
     },
