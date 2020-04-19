6de00cd4961d76b9f7c199aabda8723ad3f01232
pranapramudita <pranapramudita@gmail.com>
2020-04-19 17:16:25 +0700

    init

--- apps.js
+++ apps.js
@@ -1,10 +1,17 @@
 var app = new Vue({
     el: '#app',
     data: {
-        
+        favFood: '-',
     },
     methods: {
-        
+        readRefsFood: function(){
+            console.log(this.$refs)
+            this.favFood = this.$refs.inputFood.value
+        },
+        readRefsChoiceFood: function(){
+            console.log(this.$refs)
+            this.favFood = this.$refs.choiceFood.innerText
+        },
     },
     computed: {
         
