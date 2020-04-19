2adbcf97dc83f6251e93df217e2d4abab1e7cf64
pranapramudita <pranapramudita@gmail.com>
2020-04-19 14:52:15 +0700

    init

--- apps.js
+++ apps.js
@@ -1,12 +1,31 @@
-var app = new Vue({
-    el: '#app',
+var one = new Vue({
+    el: '#vue-app-one',
     data: {
-        
+        title: 'Vue App One'
     },
     methods: {
         
     },
     computed: {
-        
+        greet: function(){
+            return 'Hello, from app one!'
+        }
+    },
+});
+
+var two = new Vue({
+    el: '#vue-app-two',
+    data: {
+        title: 'Vue App Two'
+    },
+    methods: {
+        changeTitle: function(){
+            one.title = 'Title Changed by app two'
+        }
+    },
+    computed: {
+        greet: function(){
+            return 'Hello, from app two!'
+        }
     },
 })
