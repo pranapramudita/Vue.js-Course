7cb981acda5a0ae36cc5bf4b70a060eac247d77a
pranapramudita <pranapramudita@gmail.com>
2020-04-19 16:36:22 +0700

    init

--- apps.js
+++ apps.js
@@ -1,12 +1,21 @@
-var app = new Vue({
-    el: '#app',
-    data: {
-        
+Vue.component('greeting', {
+    template: '<p>Hey {{ name }}. <button v-on:click="changeName">Change Name</button></p>',
+    data: function(){
+        return {
+            name: 'Prana'
+        }
     },
     methods: {
-        
+        changeName: function(){
+            this.name = 'Candy'
+        }
     },
-    computed: {
-        
-    },
-})
+});
+
+new Vue({
+    el: '#vue-app-one',
+});
+
+new Vue({
+    el: '#vue-app-two',
+});
