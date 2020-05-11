da201c4032ec93d63156f8d05555236e798d1cd9
pranapramudita <pranapramudita@gmail.com>
2020-05-12 06:36:59 +0700

    init

--- hello-world/src/components/showBlogs.vue
+++ hello-world/src/components/showBlogs.vue
@@ -3,7 +3,8 @@
       <h1>All Blog Articles</h1>
       <input type="text" v-model="search" placeholder="search title blogs">
       <div v-for="(blog, index) in filteredBlogs" :key="index" class="single-blog">
-          <h3>{{ blog.title | to-uppercase }}</h3>
+          <h3 v-rainbow>{{ blog.title | to-uppercase }}</h3>
+          <!-- <h3 v-rainbow-Theme>{{ blog.title | to-uppercase }}</h3> -->
           <article>{{ blog.body | snippet }}</article>
       </div>
   </div>
@@ -33,6 +34,22 @@ export default {
              })
          }
      },
+     filters: {
+         toUppercase(value){ // method 1
+             return value.toUpperCase()
+         },
+         'snippet': function(value){ // method 2
+             return value.slice(0,100) + "..."
+         }
+     },
+     directives: {
+        rainbow: {
+        // 'rainbowTheme': {
+            bind(el){
+                el.style.color = "#" + Math.random().toString().slice(2,8)
+            }
+        }
+     }
 }
 </script>
 
