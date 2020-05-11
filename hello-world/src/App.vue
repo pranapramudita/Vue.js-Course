8f75bdf9853ce339eee1a93fb2f6cd8b41223a79
pranapramudita <pranapramudita@gmail.com>
2020-05-11 15:35:08 +0700

    init

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div>
-    <add-blog>
-
-    </add-blog>
+    <show-blogs></show-blogs>
+    <hr>
+    <add-blog></add-blog>
   </div>
 </template>
 
 <script>
 import addBlog from './components/addBlog'
+import showBlogs from './components/showBlogs'
 
 export default {
   components:{
     'add-blog': addBlog,
+    'show-blogs': showBlogs,
   }
 }
 </script>
