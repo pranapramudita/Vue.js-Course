346df91f24bb4de23fd02301c05a3bd95e5395c5
pranapramudita <pranapramudita@gmail.com>
2020-05-11 20:57:51 +0700

    init

--- hello-world/src/components/showBlogs.vue
+++ hello-world/src/components/showBlogs.vue
@@ -1,9 +1,9 @@
 <template>
-  <div v-theme:column="'narrow'" id="show-blogs">
+  <div id="show-blogs">
       <h1>All Blog Articles</h1>
       <div v-for="(blog, index) in blogs" :key="index" class="single-blog">
-          <h3 v-rainbow>{{ blog.title }}</h3>
-          <article>{{ blog.body }}</article>
+          <h3>{{ blog.title | to-uppercase }}</h3>
+          <article>{{ blog.body | snippet }}</article>
       </div>
   </div>
 </template>
--- hello-world/src/main.js
+++ hello-world/src/main.js
@@ -4,25 +4,13 @@ import VueResource from 'vue-resource'
 
 Vue.use(VueResource)
 
-// Custom Directives
-Vue.directive('rainbow', {
-  bind(el){
-    el.style.color = "#" + Math.random().toString().slice(2,8)
-  }
+// Filters
+Vue.filter('to-uppercase', function(value){
+  return value.toUpperCase()
 })
 
-Vue.directive('theme', {
-  bind(el, binding){
-    if(binding.value == 'wide'){
-      el.style.maxWidth = "1500px"
-    }else if(binding.value == 'narrow'){
-      el.style.maxWidth = "560px"
-    }
-    if(binding.arg == 'column'){
-      el.style.background = '#ddd'
-      el.style.padding = '20px'
-    }
-  }
+Vue.filter('snippet', function(value){
+  return value.slice(0,100) + "..."
 })
 
 Vue.config.productionTip = false
