843b737f5ef000d783aae31316f4965d957500e5
pranapramudita <pranapramudita@gmail.com>
2020-05-12 12:25:26 +0700

    init

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -1,23 +1,13 @@
 <template>
   <div>
-    <show-blogs></show-blogs>
-    <hr>
-    <list-blogs></list-blogs>
-    <hr>
-    <add-blog></add-blog>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import addBlog from './components/addBlog'
-import showBlogs from './components/showBlogs'
-import listBlogs from './components/listBlogs'
-
 export default {
   components:{
-    'add-blog': addBlog,
-    'show-blogs': showBlogs,
-    'list-blogs': listBlogs,
+
   }
 }
 </script>
--- hello-world/src/main.js
+++ hello-world/src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueResource from 'vue-resource'
+import VueRouter from 'vue-router'
+import Routes from './routes'
 
 Vue.use(VueResource)
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  routes: Routes
+})
 
 Vue.config.productionTip = false
 
@@ -10,4 +17,5 @@ export const bus = new Vue()
 
 new Vue({
   render: h => h(App),
+  router: router,
 }).$mount('#app')
