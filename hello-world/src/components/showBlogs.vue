e168fc14ee133200912ed49ad1ec4eeb064f4546
pranapramudita <pranapramudita@gmail.com>
2020-05-12 19:12:43 +0700

    init

--- hello-world/src/components/showBlogs.vue
+++ hello-world/src/components/showBlogs.vue
@@ -1,52 +1,58 @@
 <template>
   <div id="show-blogs">
-      <h1>All Blog Articles</h1>
-      <input type="text" v-model="search" placeholder="search title blogs">
-      <div v-for="(blog, index) in filteredBlogs" :key="index" class="single-blog">
-          <router-link v-bind:to="'/blog/' + blog.id"><h3 v-rainbow>{{ blog.title | to-uppercase }}</h3></router-link>
-          <article>{{ blog.body | snippet }}</article>
-      </div>
-  </div>
+        <h1>All Blog Articles</h1>
+        <input type="text" v-model="search" placeholder="search title blogs">
+        <div v-for="(blog, index) in filteredBlogs" :key="index" class="single-blog">
+            <router-link v-bind:to="'/blog/' + blog.id"><h3 v-rainbow>{{ blog.title | to-uppercase }}</h3></router-link>
+            <article>{{ blog.content | snippet }}</article>
+        </div>
+    </div>
 </template>
 
 <script>
 import searchMixin from '../mixins/searchMixin'
 
 export default {
-     data() {
-         return {
-             blogs: [],
-             search: '',
-         }
-     },
-     methods: {
+    data() {
+        return {
+            blogs: [],
+            search: '',
+        }
+    },
+    methods: {
          
-     },
-     created() {
-         this.$http.get('https://jsonplaceholder.typicode.com/posts').then(function(data){
-             console.log(data)
-             this.blogs = data.body.slice(0,5)
-         })
-     },
-     computed: {
+    },
+    created() {
+        this.$http.get('https://vue-js-course-f0607.firebaseio.com/posts.json').then(function(data){
+            return data.json()
+        }).then(function(data){
+            var blogsArray = []
+            for(let key in data){
+                data[key].id = key
+                blogsArray.push(data[key])
+            }
+            this.blogs = blogsArray
+        })
+    },
+    computed: {
 
-     },
-     filters: {
-         toUppercase(value){
-             return value.toUpperCase()
-         },
+    },
+    filters: {
+        toUppercase(value){
+            return value.toUpperCase()
+        },
         snippet(value){
-             return value.slice(0,100) + "..."
-         }
-     },
-     directives: {
+            return value.slice(0,100) + "..."
+        }
+    },
+    directives: {
         rainbow: {
             bind(el){
                 el.style.color = "#" + Math.random().toString().slice(2,8)
             }
         }
-     },
-     mixins: [searchMixin]
+    },
+    mixins: [searchMixin]
 }
 </script>
 
--- hello-world/src/components/singleBlog.vue
+++ hello-world/src/components/singleBlog.vue
@@ -1,7 +1,11 @@
 <template>
   <div id="single-blog">
-      <h1>{{ blog.title }}</h1>
-      <article>{{ blog.body }}</article>
+    <h1>{{ blog.title }}</h1>
+    <article>{{ blog.content }}</article>
+    <p>Author: {{ blog.author }}</p>
+    <ul>
+        <li v-for="(category, index) in blog.categories" :key="index">{{ category }}</li>
+    </ul>
   </div>
 </template>
 
@@ -14,9 +18,10 @@ export default {
         }
     },
     created() {
-        this.$http.get('https://jsonplaceholder.typicode.com/posts/' + this.id).then(function(data) {
-            console.log(data)
-            this.blog = data.body
+        this.$http.get('https://vue-js-course-f0607.firebaseio.com/posts/' + this.id + '.json').then(function(data) {
+            return data.json()
+        }).then(function(data){
+            this.blog = data
         })
     },
 }
