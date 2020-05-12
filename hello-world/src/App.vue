106ba67285ed9bbc82a96d1ec9ee20b882585a0c
pranapramudita <pranapramudita@gmail.com>
2020-05-12 13:06:04 +0700

    init

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -1,19 +1,23 @@
 <template>
   <div>
+    <app-header></app-header>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import header from './components/header'
+
 export default {
   components:{
-
+    'app-header': header,
   }
 }
 </script>
 
 <style>
 body{
+  margin: 0px;
   font-family: Roboto;
 }
 </style>
